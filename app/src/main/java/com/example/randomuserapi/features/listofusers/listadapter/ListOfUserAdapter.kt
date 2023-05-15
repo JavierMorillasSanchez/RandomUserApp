be35756fd83183a3505b9e5a558584d94db11712
JavierMS <morillas.sanchez.91@gmail.com>
2023-05-15 09:23:18 +0200

    Added image to cell list using glide

--- app/src/main/java/com/example/randomuserapi/features/listofusers/listadapter/ListOfUserAdapter.kt
+++ app/src/main/java/com/example/randomuserapi/features/listofusers/listadapter/ListOfUserAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.randomuserapi.features.listofusers.listadapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.randomuserapi.R
 import com.example.randomuserapi.calls.usecaseclasses.randomuserclass.RandomUser
 
@@ -26,11 +29,19 @@ class ListOfUserAdapter (
         holder.cellUserName.text = randomUserList[position].getRandomUserFullName()
         holder.cellUserPhone.text = randomUserList[position].phone
         holder.cellUserMail.text = randomUserList[position].email
+        loadImages(randomUserList[position].pictureThumbnail, holder.cellImg, holder.itemView.context)
 
         holder.itemView.setOnClickListener {
             Toast.makeText(holder.itemView.context, "Has pulsado a ${holder.cellUserName.text}", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    fun loadImages(url: String?, userImg: ImageView, context: Context) {
+        Glide.with(context)
+            .load(url)
+            .error(R.drawable.default_user)
+            .into(userImg)
     }
 
 }
