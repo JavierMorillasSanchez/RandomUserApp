2457ad1af202e43a6d3cdb0a60b7667a1f72ee9d
JavierMS <morillas.sanchez.91@gmail.com>
2024-05-30 13:52:55 +0200

    Added new test - WIP

--- app/src/test/java/com/example/randomuserapi/features/listofusers/listofusersviewmodel/ListOfUsersViewModelTest.kt
+++ app/src/test/java/com/example/randomuserapi/features/listofusers/listofusersviewmodel/ListOfUsersViewModelTest.kt
@@ -1,21 +1,62 @@
 package com.example.randomuserapi.features.listofusers.listofusersviewmodel
 
-import com.example.randomuserapi.calls.data.RandomUserRepository
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.randomuserapi.calls.domain.GetRandomUserUseCase
 import com.example.randomuserapi.calls.domain.model.RandomUser
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
-import io.mockk.coVerify
 import io.mockk.impl.annotations.RelaxedMockK
-import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.BeforeEach
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
 
 
+@ExperimentalCoroutinesApi
 class ListOfUsersViewModelTest{
 
+    @RelaxedMockK
+    private lateinit var randomUserUseCase: GetRandomUserUseCase
 
+    private lateinit var viewModel: ListOfUsersViewModel
 
+    @get:Rule
+    var rule: InstantTaskExecutorRule = InstantTaskExecutorRule()
 
+    @BeforeEach
+    fun onBefore(){
+        MockKAnnotations.init(this)
+        viewModel = ListOfUsersViewModel(randomUserUseCase)
+        Dispatchers.setMain(Dispatchers.Unconfined)
+    }
+
+    @After
+    fun onAfter(){
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `when the api call results on a null user, call notAllUsersRecieved method`() = runTest {
+
+        val numberOfTimesApiCalled = 1
+        val nullRandomUser: RandomUser? = null
+        val allUsersRecievedValue = viewModel.checkIfAllUsersHasBeenRecieved().value
+
+        //Given
+        coEvery { randomUserUseCase.getRandomUserFromApi() } returns nullRandomUser
+
+        //When
+        viewModel.getRandomUserListFromApiCall(numberOfTimesApiCalled)
+
+        //Then
+        assert(allUsersRecievedValue == false)
+
+    }
 
 }
