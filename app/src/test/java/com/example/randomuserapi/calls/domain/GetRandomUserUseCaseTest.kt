538afee5aaddb6c726c275d968c0d5fe302b2570
JavierMS <morillas.sanchez.91@gmail.com>
2024-05-30 10:55:10 +0200

    Added libraries and get random user use case test

---
+++ app/src/test/java/com/example/randomuserapi/calls/domain/GetRandomUserUseCaseTest.kt
@@ -0,0 +1,42 @@
+package com.example.randomuserapi.calls.domain
+
+import com.example.randomuserapi.calls.data.RandomUserRepository
+import com.example.randomuserapi.calls.domain.model.RandomUser
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class GetRandomUserUseCaseTest{
+
+    @RelaxedMockK
+    private lateinit var repository: RandomUserRepository
+
+    private lateinit var getRandomUserUseCase: GetRandomUserUseCase
+
+    @BeforeEach
+    fun onBefore(){
+        MockKAnnotations.init(this)
+        this.getRandomUserUseCase = GetRandomUserUseCase(repository)
+    }
+
+    @Test
+    fun `when making a single call, the api returns null user`() = runBlocking{
+
+        val userRecieved: RandomUser? = null
+        //Given
+        coEvery { repository.getRandomUserFromApi() } returns userRecieved
+
+        //Then
+        val response = getRandomUserUseCase.getRandomUserFromApi()
+
+        //When
+        assert(userRecieved == response )
+
+    }
+
+
+
+}
---
+++ app/src/test/java/com/example/randomuserapi/features/listofusers/listofusersviewmodel/ListOfUsersViewModelTest.kt
@@ -0,0 +1,21 @@
+package com.example.randomuserapi.features.listofusers.listofusersviewmodel
+
+import com.example.randomuserapi.calls.data.RandomUserRepository
+import com.example.randomuserapi.calls.domain.GetRandomUserUseCase
+import com.example.randomuserapi.calls.domain.model.RandomUser
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+
+class ListOfUsersViewModelTest{
+
+
+
+
+
+}
