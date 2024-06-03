9177d5a8a29a6b3b9fdd81ac7f51061a05c965c4
JavierMS <morillas.sanchez.91@gmail.com>
2024-06-03 11:03:41 +0200

    Added test to view model

--- app/src/test/java/com/example/randomuserapi/features/listofusers/listofusersviewmodel/ListOfUsersViewModelTest.kt
+++ app/src/test/java/com/example/randomuserapi/features/listofusers/listofusersviewmodel/ListOfUsersViewModelTest.kt
@@ -5,6 +5,7 @@ import androidx.arch.core.executor.TaskExecutor
 import com.example.randomuserapi.calls.data.RandomUserRepository
 import com.example.randomuserapi.calls.domain.GetRandomUserUseCase
 import com.example.randomuserapi.calls.domain.model.RandomUser
+import com.example.randomuserapi.utils.MockedUser
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.RelaxedMockK
@@ -14,7 +15,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
-import org.junit.After
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -65,24 +66,77 @@ class ListOfUsersViewModelTest{
         Dispatchers.setMain(Dispatchers.Unconfined)
     }
 
-    @After
+    @AfterEach
     fun onAfter(){
         Dispatchers.resetMain()
     }
 
     @Test
-    fun `when the api call results on a null user, call checkIfAllUsersHasBeenRecieved method`() = runTest {
+    fun `when the api call results on a null user, the user is not stored in randomUserList`() = runTest {
+
+        val nullRandomUserRecieved = null
 
         //Given
         coEvery {
             randomUserUseCase()
-            viewModelMock.addUserRecievedToUserList(null)
+            viewModelMock.addUserRecievedToUserList(nullRandomUserRecieved)
         }
-
-        //When
 
         //Then
         assertTrue(viewModelMock.getRandomUserList().isEmpty())
     }
+
+    @Test
+    fun `when the api call results on a non null user, the user is stored in randomUserList`() = runTest {
+
+        val randomUserRecieved = MockedUser.mockedUser
+
+        //Given
+        coEvery {
+            randomUserUseCase()
+            viewModelMock.addUserRecievedToUserList(randomUserRecieved)
+        }
+
+        //Then
+        assertTrue(viewModelMock.getRandomUserList().isNotEmpty())
+    }
+
+    @Test
+    fun `when the user list comes from Database, if there is no users, the variable userListPrepared is false`() = runTest {
+
+        val userListNotPrepared = false
+        val emptyUserListFromDatabase: ArrayList<RandomUser> = ArrayList()
+
+        //Given
+        coEvery {
+            repository.getRandomUserFromDatabase()
+        } returns emptyUserListFromDatabase
+
+        //When
+        viewModelMock.getRandomUserListFromDatabase()
+
+        //Then
+        assert(userListNotPrepared == viewModelMock.getUserListPreparedValue().value)
+    }
+
+    @Test
+    fun `when the user list comes from Database, if there is at least one user, the variable userListPrepared is true`() = runTest {
+
+        val userListPrepared = true
+        val userListFromDatabase: ArrayList<RandomUser> = ArrayList()
+        MockedUser.mockedUser?.let { userListFromDatabase.add(it) }
+
+        //Given
+        coEvery {
+            repository.getRandomUserFromDatabase()
+        } returns userListFromDatabase
+
+        //When
+        viewModelMock.getRandomUserListFromDatabase()
+
+        //Then
+        assert(userListPrepared == viewModelMock.getUserListPreparedValue().value)
+    }
+
 
 }
